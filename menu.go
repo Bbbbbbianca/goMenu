068bd99f2e2820a769b7f5b878e3d779a7b4688c
Bbbbbbianca <1104191069@qq.com>
2023-03-20 15:27:43 +0800

    update

--- menu.go
+++ menu.go
@@ -1,10 +1,12 @@
 package main
+
 import "fmt"
- 
+
 func main() {
 	for {
 		var cmd string
-		fmt.Scan(&cmd)
+		fmt.Print("MENU: \n 1. Welcome. \n 2. Help. \n 3. Quit.\n")
+		fmt.Scanln(&cmd)
 		if cmd == "3" {
 			break
 		}
@@ -12,7 +14,7 @@ func main() {
 		case "1":
 			fmt.Println("Hello world!")
 		case "2":
-			fmt.Println("Help command:")
+			fmt.Println("Help command")
 		default:
 			fmt.Println("Unknown command")
 		}
